8ca60093c9e8b543aaac52164186624084026e23
NetHunter123 <viktordmitryk3@gmail.com>
2022-08-16 11:48:40 +0300

    add svgr

--- layout/Footer/Footer.jsx
+++ layout/Footer/Footer.jsx
@@ -1,5 +1,6 @@
 import styles from "./Footer.module.css";
+import ArrowIcon from "./chevron-rt.svg";
 
 export const Footer = ({ ...props }) => {
-  return <footer className={'container'} {...props}>Footer</footer>;
+  return <footer className={'container'} {...props}>Footer<span><ArrowIcon/></span></footer>;
 };
--- next.config.js
+++ next.config.js
@@ -2,6 +2,33 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  webpack(config, options) {
+    config.module.rules.push({
+      use: [
+        {
+          loader: "@svgr/webpack",
+          options: {
+            prettier: false,
+            svgo: true,
+            svgoConfig: {
+              plugins: [
+                {
+                  name: "removeViewBox",
+                  params: {
+                    attrs: "false",
+                  },
+                },
+              ],
+            },
+            titleProp: true,
+          },
+        },
+      ],
+      test: /\.svg$/,
+    });
 
-module.exports = nextConfig
+    return config;
+  },
+};
+
+module.exports = nextConfig;
