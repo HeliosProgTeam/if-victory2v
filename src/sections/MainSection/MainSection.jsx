35715f6761acf7c67d9bc309847640ee3e1f327e
NetHunter123 <viktordmitryk3@gmail.com>
2022-08-19 13:46:26 +0300

    remake home page

--- src/sections/MainSection/MainSection.jsx
+++ src/sections/MainSection/MainSection.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import CharityImg from "./charity.jpg";
+import CharityImg from "./bg.jpg";
 import ImageNext from "next/image";
 import Image from "../../components/Image";
 
@@ -7,21 +7,41 @@ export const MainSection = ({ ...props }) => {
   return (
     <section className="home" {...props}>
       <div className="home__bg">
-        <div className="home__info">
-          <div className="home__info-inner">
-            <h2 className="home__subtitle">Вiтя гей</h2>
-            <h1 className="home__title">Благодiйний фонд IФ-ВIКТОРI</h1>
-            <p className="home__desc">
-              Lorem ipsum dolor sit amet, consectetur
-              adipiscing elit.
-            </p>
+        <div className="home__info-wrapper">
+          <div className="home__info">
+            <div className="home__info-inner">
+              {/*<h2 className="home__subtitle">Вiтя гей</h2>*/}
+              <h1 className="home__title">Благодiйний фонд IФ-ВIКТОРI</h1>
+              <p className="home__desc">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              </p>
+            </div>
           </div>
         </div>
-        <div className="home__img">
+        <div className="home__img-bg">
           {/*<Image width={300} height={200} src={MainImg} alt="image"/>*/}
           <ImageNext src={CharityImg} alt="image" />
         </div>
+        <div className="home__bottom"></div>
       </div>
     </section>
+    // <section className="home" {...props}>
+    //   <div className="home__bg">
+    //     <div className="home__info">
+    //       <div className="home__info-inner">
+    //         <h2 className="home__subtitle">Вiтя гей</h2>
+    //         <h1 className="home__title">Благодiйний фонд IФ-ВIКТОРI</h1>
+    //         <p className="home__desc">
+    //           Lorem ipsum dolor sit amet, consectetur
+    //           adipiscing elit.
+    //         </p>
+    //       </div>
+    //     </div>
+    //     <div className="home__img">
+    //       {/*<Image width={300} height={200} src={MainImg} alt="image"/>*/}
+    //       <ImageNext src={CharityImg} alt="image" />
+    //     </div>
+    //   </div>
+    // </section>
   );
 };
