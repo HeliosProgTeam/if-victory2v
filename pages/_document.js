c1dc4ed4122a970efb7cb8e29cd2dae294feac6a
NetHunter123 <viktordmitryk3@gmail.com>
2022-08-21 18:06:26 +0300

    add modal content

--- pages/_document.js
+++ pages/_document.js
@@ -30,7 +30,7 @@ export default class MyDocument extends Document {
           {/* Fonts */}
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link href="https://fonts.googleapis.com/css2?family=Manrope:wght@500;800&display=swap" rel="stylesheet"/>
-          <link href="https://fonts.googleapis.com/css2?family=Jost:wght@800&display=swap" rel="stylesheet"/>
+          <link href="https://fonts.googleapis.com/css2?family=Jost:wght@500;800&display=swap" rel="stylesheet"/>
           <link href="https://fonts.googleapis.com/css2?family=Crimson+Pro&display=swap" rel="stylesheet"/>
         </Head>
 
---
+++ src/components/bankAccounts/BankAccounts.jsx
@@ -0,0 +1,87 @@
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import Modal from "@mui/material/Modal";
+import BankAccountsTable from "../BankAccountsTable/BankAccountsTable";
+import Img from "../Image";
+import StampImg from "./stamp.jpg";
+import CloseIcon from "@mui/icons-material/Close";
+const style = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  height: "95%",
+  width: "90%",
+  bgcolor: "background.paper",
+  border: "2px solid #000",
+  boxShadow: 24,
+  padding: "10px",
+  paddingTop: "10px",
+  overflowY: "scroll",
+};
+
+export default function BankAccounts({ className }) {
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
+  return (
+    <div className={"bank-accounts"}>
+      <Button onClick={handleOpen} className={className}>
+        Реквiзити
+      </Button>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <div className="bank-accounts__header">
+            <Button className="bank-accounts__btn-close" onClick={handleClose}>
+              <CloseIcon />
+            </Button>
+          </div>
+
+          <div className="bank-accounts__wrap">
+            <div className="bank-accounts__top">
+              <div className="bank-accounts__top-info">
+                <h2 className={"bank-accounts__title"}>
+                  Реквізити рахунків клієнта, Відкритих у {'"Кредобанк"'}
+                </h2>
+                <h3 className={"bank-accounts__top-text"}>
+                  <p
+                    className={
+                      "bank-accounts__сolumn bank-accounts__сolumn--head"
+                    }
+                  >
+                    Клієнт:
+                  </p>
+                  <p className={"bank-accounts__сolumn"}>
+                    Благодійна Організація {'"Благодійний Фонд "ІФ-ВІКТОРІ""'}
+                  </p>
+                </h3>
+                <p className={"bank-accounts__top-text"}>
+                  <p
+                    className={
+                      "bank-accounts__сolumn bank-accounts__сolumn--head"
+                    }
+                  >
+                    Ідент. код:
+                  </p>
+                  <p>44763366</p>
+                </p>
+              </div>
+              <div className="bank-accounts__stamp">
+                <Img src={StampImg} alt={"Штамп"} />
+              </div>
+            </div>
+            <BankAccountsTable />
+          </div>
+        </Box>
+      </Modal>
+    </div>
+  );
+}
