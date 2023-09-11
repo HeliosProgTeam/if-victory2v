a2c84b55c240756b799441bd9284fd4ad9432eda
TellFruit <ju1c7.d1g1t2@gmail.com>
2023-09-11 14:45:01 +0300

    change owner labels from devs to foundation

--- src/components/developersMedia/DevelopersMedia.jsx
+++ src/components/developersMedia/DevelopersMedia.jsx
@@ -5,14 +5,14 @@ const FoundationMedia = () => {
   return (
     <div className={"foundation"}>
       <span className={"foundation__rights"}>
-        © {new Date().getFullYear()} Helios, Inc. All Rights reserved
+        © {new Date().getFullYear()} БО &quot;ПРИКАРПАТГАРАНТ&quot; | All Rights reserved
       </span>
       <div className="foundation__media">
         <div className="foundation__item">
           <a
             href="https://www.facebook.com/profile.php?id=100091625511309"
             className={"foundation__icon "}
-            aria-label={"Фейсбук розробника"}
+            aria-label={"Фейсбук фонду"}
           >
             <FacebookIcon style={{ fontSize: "inherit" }} />
           </a>
