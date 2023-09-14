9c2ab9339e7d5526e1e13ec3987169debee6930c
TellFruit <ju1c7.d1g1t2@gmail.com>
2023-09-14 09:30:56 +0300

    create privacy policy page

---
+++ pages/privacy-policy/index.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { withLayout } from "../../src/layout/Layout";
+
+const PrivacyPolicyPage = () => {
+    return (
+        <div></div>
+    );
+  };
+  
+  export default withLayout(PrivacyPolicyPage);
