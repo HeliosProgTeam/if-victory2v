586a4e7117d655836f3da23a7a8b4116579dd862
TellFruit <ju1c7.d1g1t2@gmail.com>
2023-09-15 14:47:08 +0300

    create styles for blog item component

--- src/components/blogItem/BlogItem.jsx
+++ src/components/blogItem/BlogItem.jsx
@@ -4,10 +4,10 @@ import Img from "../Image";
 const BlogItem = ({ imgPath, title, text, date }) => {
   <div className="blog-item">
     <Img src={imgPath} alt={title} className="blog-item__image" />
-    <div className="blog-item__content">
-      <h2 className="blog-item__title">{title}</h2>
-      <p className="blog-item__text">{text}</p>
-      <p className="blog-item__date">{date}</p>
+    <div className="blog-item-content">
+      <h2 className="blog-item-content__title">{title}</h2>
+      <p className="blog-item-content__text">{text}</p>
+      <p className="blog-item-content__date">{date}</p>
     </div>
   </div>;
 };
