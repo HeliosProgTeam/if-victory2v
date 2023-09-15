3c9d5960eaf0be976ce1727dc50a57ee8a111313
TellFruit <ju1c7.d1g1t2@gmail.com>
2023-09-15 17:00:42 +0300

    add id to blog item entity

--- src/blogList/BlogList.jsx
+++ src/blogList/BlogList.jsx
@@ -4,9 +4,9 @@ import BlogItem from "../components/blogItem/blogItem";
 const BlogList = ({ blogItems }) => {
   return (
     <div className="blog-list">
-      {blogItems.map((item, index) => (
+      {blogItems.map((item) => (
         <BlogItem
-          key={index}
+          key={item.id}
           imgPath={item.imgPath}
           title={item.title}
           text={item.text}
