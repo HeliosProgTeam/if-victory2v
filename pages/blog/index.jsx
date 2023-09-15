ed83e99cf7f3d93de8c4a8cf6b35a6ee629cd265
TellFruit <ju1c7.d1g1t2@gmail.com>
2023-09-15 14:53:01 +0300

    create blog page component with sample data

---
+++ pages/blog/index.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+
+const blogItems = [
+  {
+    imgPath: "blog-item-example.jpg",
+    title: "Blog Post 1",
+    text: "This is the content of Blog Post.",
+    date: "2023-09-15",
+  },
+  {
+    imgPath: "blog-item-example.jpg",
+    title: "Blog Post 1",
+    text: "This is the content of Blog Post.",
+    date: "2023-09-15",
+  },
+  {
+    imgPath: "blog-item-example.jpg",
+    title: "Blog Post 1",
+    text: "This is the content of Blog Post.",
+    date: "2023-09-15",
+  },
+  {
+    imgPath: "blog-item-example.jpg",
+    title: "Blog Post 1",
+    text: "This is the content of Blog Post.",
+    date: "2023-09-15",
+  },
+  {
+    imgPath: "blog-item-example.jpg",
+    title: "Blog Post 1",
+    text: "This is the content of Blog Post.",
+    date: "2023-09-15",
+  }
+];
+
+const BlogPage = () => {
+  return (
+    <div className="blog-page">
+      <h1 className="blog-page__title">Блог фонду</h1>
+      <BlogList blogItems={blogItems} />
+    </div>
+  );
+};
+
+export default BlogPage;
