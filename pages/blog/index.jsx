7e74b7c6a09a2975d791e97bc837adab584958f8
TellFruit <ju1c7.d1g1t2@gmail.com>
2023-09-24 14:55:38 +0300

    add second blog post

--- pages/blog/index.jsx
+++ pages/blog/index.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { withLayout } from "../../src/layout/Layout";
 import BlogList from "../../src/components/blogList/BlogList";
 import BlogImageOne from "../../public/images/blog_image_1.webp";
+import BlogImageTwo from "../../public/images/blog_image_2.webp";
 
 const blogItems = [
   {
@@ -10,6 +11,13 @@ const blogItems = [
     title: "Прикарпат Гарант. Історія створення організації та наша мета.",
     text: "Йшов пятий місяць війни. Я, Лікаренко Петро Васильович, реєструю свою благодійну організацію, щоб мати можливість допомогти країні.",
     date: "16.09.2023",
+  },
+  {
+    id: 2,
+    img: BlogImageTwo,
+    title: "Аграрний сектор України.",
+    text: "Організація співпрацію з двома виробниками. Виробник мікродобрив: ТЗОВ «ЕКОХІМІФ» ти виробник пального та паливних матеріалів: ПП «ПРИКАРПАТКОМПЛЕКТ».",
+    date: "20.09.2023",
   }
 ];
 
