b86e49e32a8f5f29d2c1f47d748b7086a238403f
TellFruit <ju1c7.d1g1t2@gmail.com>
2023-09-26 19:00:53 +0300

    add 3rd 4th 5th blog post cards

--- pages/blog/index.jsx
+++ pages/blog/index.jsx
@@ -3,6 +3,9 @@ import { withLayout } from "../../src/layout/Layout";
 import BlogList from "../../src/components/blogList/BlogList";
 import BlogImageOne from "../../public/images/blog_image_1.webp";
 import BlogImageTwo from "../../public/images/blog_image_2.webp";
+import BlogImageThree from "../../public/images/blog_image_3.webp";
+import BlogImageFour from "../../public/images/blog_image_4.webp";
+import BlogImageFive from "../../public/images/blog_image_5.webp";
 
 const blogItems = [
   {
@@ -14,11 +17,32 @@ const blogItems = [
   },
   {
     id: 2,
-    img: BlogImageTwo,
-    title: "Аграрний сектор України.",
-    text: "Організація співпрацію з двома виробниками. Виробник мікродобрив: ТЗОВ «ЕКОХІМІФ» ти виробник пального та паливних матеріалів: ПП «ПРИКАРПАТКОМПЛЕКТ».",
+    img: BlogImageThree,
+    title: "Біженці в сьогоденній політиці. ",
+    text: "Проблема біженців є питанням номер один у всіх державах світу, а також їхнє правове положення, визначення статусу міжнародних документів, вирішенню питань в нормативному визначенні поняття «біженець».",
     date: "20.09.2023",
-  }
+  },
+  {
+    id: 3,
+    img: BlogImageTwo,
+    title: "Аграрний сектор України. ",
+    text: "Організація співпрацію з двома виробниками. Виробник мікродобрив: ТЗОВ «ЕКОХІМІФ» ти виробник пального та паливних матеріалів: ПП «ПРИКАРПАТКОМПЛЕКТ».",
+    date: "24.09.2023",
+  },
+  {
+    id: 4,
+    img: BlogImageFour,
+    title: "Наші цінності. ",
+    text: "Наша місія ґрунтується на низці основних цінностей, якими керуємося в усіх наших діях: емпатія, співпраця, сталість, прозорість, інновації.",
+    date: "25.09.2023",
+  },
+  {
+    id: 5,
+    img: BlogImageFive,
+    title: "Розширення можливостей спільнот. ",
+    text: "Ми віримо в внутрішній потенціал кожної спільноти для створення позитивних змін. Наш підхід полягає в наданні інструментів, ресурсів і підтримки, необхідних для розкриття цього потенціалу.",
+    date: "25.09.2023",
+  },
 ];
 
 const BlogPage = () => {
