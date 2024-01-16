1643f9d4d52c5fb54a433412a007b5005827736f
OlegBW <olegkorostilov58@gmail.com>
2024-01-16 03:12:58 +0200

    feat: Update some About styles

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,13 @@
 import { withLayout } from "../src/layout/Layout";
 import { MainSection } from "../src/sections/MainSection/MainSection";
 import About from "../src/sections/About/About";
-import Contacts from "../src/sections/Contacts/Contacts";
+// import Contacts from "../src/sections/Contacts/Contacts";
 
 function Home() {
   return (
     <>
       <MainSection />
       <About />
-      <Contacts />
     </>
   );
 }
