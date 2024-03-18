70effe98e6416768a774363fe703e760147f99cc
OlegBW <olegkorostilov58@gmail.com>
2024-03-18 14:47:23 +0200

    feat: Adding new icons

--- src/components/SocialLinks/SocialLinks.jsx
+++ src/components/SocialLinks/SocialLinks.jsx
@@ -1,15 +1,15 @@
 import InstagramLogo from '../../../public/images/instagram.svg';
-import FacebookLogo from '../../../public/images/instagram.svg';
+import FacebookLogo from '../../../public/images/facebook.svg';
 
 export default function SocialLinks() {
     console.log(InstagramLogo);
     return (
         <div className='links-wrapper'>
             <a href="https://www.facebook.com/share/1XGxQAEjNGLi7CFh/?mibextid=dGKdO6">
-                <img src={FacebookLogo} alt="facebook-logo" />
+                <img src={FacebookLogo.src} alt="facebook-logo" />
             </a>
             <a href="https://www.instagram.com/prykarpat_garant_">
-                <img src={InstagramLogo} alt="instagram-logo" />
+                <img src={InstagramLogo.src} alt="instagram-logo" />
             </a>
         </div>
     )
