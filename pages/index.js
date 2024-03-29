f2a2ecd78a0b9fc6295cad5c25dfbcc7027f0b43
OlegBW <olegkorostilov58@gmail.com>
2024-03-29 18:18:19 +0200

    feat: Adjustments

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import { MainSection } from "../src/sections/MainSection/MainSection";
 import About from "../src/sections/About/About";
 // import Contacts from "../src/sections/Contacts/Contacts";
 import Activities from "../src/sections/Activities/Activities.jsx";
-import SocialBlock from "../src/components/SocialBlock/SocialBlock.jsx";
+import SocialBlock from "../src/sections/SocialNetworks/SocialNetworks.jsx";
 
 function Home() {
   return (
